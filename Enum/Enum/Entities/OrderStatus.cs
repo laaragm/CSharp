203fb5ee9acca76a5c558096e3ca60ca3a6141e7
Lara Galvani <41298512+laaragm@users.noreply.github.com>
2020-05-17 11:11:26 -0300

    Add files via upload

---
+++ Enum/Enum/Entities/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enum.Entities
+{
+    //An enum is a special "class" that represents a group of constants (unchangeable/read-only
+    //variables).
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,24 @@
+﻿using Enum.Entities;
+using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 743235,
+                Moment = DateTime.Now,
+                Status = OrderStatus.Delivered
+            };
+
+            Console.WriteLine(order);
+
+            //enum to string
+            //Console.WriteLine(OrderStatus.PendingPayment.ToString());
+
+        }
+    }
+}
