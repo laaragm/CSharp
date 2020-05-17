53a81db701789d9357fe4c0eee91184d5ad42d67
Lara Galvani <41298512+laaragm@users.noreply.github.com>
2020-05-17 12:30:59 -0300

    Add files via upload

---
+++ Composition2/Composition2/Entities/Post.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composition2.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments = new List<Comment>();
+
+        public Post()
+        {
+
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            //StringBuilder is a class that is useful to represent a mutable string of characters
+            //and it is an object of System.Text namespace. Both string and StringBuilder will
+            //represent a sequence of characters and perform the same kind of operations but the only
+            //difference is strings are immutable and StringBuilder is mutable.
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine(Title);
+            stringBuilder.Append(Likes);
+            stringBuilder.Append(" Likes - ");
+            stringBuilder.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            stringBuilder.AppendLine(Content);
+            stringBuilder.AppendLine("Comments:");
+            foreach (Comment comment in Comments)
+            {
+                stringBuilder.AppendLine(comment.Text);
+            }
+            return stringBuilder.ToString();
+
+        }
+
+    }
+}
---
+++ Composition2/Composition2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Composition2.Entities;
+
+namespace Composition2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Post p1 = new Post(
+                    DateTime.Parse("17/05/2020 12:20:15"), //date
+                    "Traveling to Iceland", //title
+                    "Can't wait to visit this wonderful country!", //content
+                    12); //likes
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("Enjoy!");
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Post p2 = new Post(
+                    DateTime.Now,
+                    "Tv Shows",
+                    "I need to find a new tv show",
+                    5);
+            Comment c3 = new Comment("I'm pretty sure you'll love Game of Thrones");
+            Comment c4 = new Comment("You should study C# lol");
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
