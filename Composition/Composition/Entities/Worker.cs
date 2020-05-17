7f8ed57b0ba04d3ba130782234a2f9feec371621
Lara Galvani <41298512+laaragm@users.noreply.github.com>
2020-05-17 12:28:59 -0300

    Add files via upload

---
+++ Composition/Composition/Entities/Worker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composition.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> HourContract { get; private set; } = new List<HourContract>();
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+            HourContract = new List<HourContract>();
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            HourContract.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            HourContract.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in HourContract)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+
+            return sum;
+        }
+
+    }
+}
